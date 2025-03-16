e16947060d998a75cd451a2e2077a214d37d3dde
umarhunter <markufaruque@gmail.com>
2025-03-16 01:16:16 -0400

    fix: general updates to code

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -1,7 +1,19 @@
+#data_loader.py is a script that loads the data from the csv files and returns the data in the form of a pandas dataframe.
+import os
+import sys
 import pandas as pd
-# data_loader.py is a script that loads the data from the csv files and returns the data in the form of a pandas dataframe.
+
+
 
 def load_medicare_data():
     # Load the data from the csv files
-    medicare_data = pd.read_csv('data/insurance/medicare/2022/medicare.csv', dtype='str')
+    # Get the project root directory (2 levels up from notebook)
+    PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+
+    # Construct the path to the data file
+    # data_path = os.path.join(PROJECT_ROOT, 'vdba', 'data', 'insurance', 'medicare', '2022', 'medicare.csv')
+    data_path = os.path.join(PROJECT_ROOT, 'vdba', 'data', 'processed', 'sample_ny_data.csv')
+    medicare_data = pd.read_csv(data_path, dtype='str').head(100)
+    # ny_data = medicare_data[medicare_data['Rndrng_Prvdr_State_Abrvtn'] == 'NY']
+    # ny_data_sample = ny_data.sample(n=50000, random_state=42)
     return medicare_data
