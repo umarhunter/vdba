50c146f80ac82b328fe6a53297a4e882e7882a6b
umarhunter <markufaruque@gmail.com>
2025-04-01 00:07:22 -0400

    fix: add ngrok token file to gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 # __init__.py
+import os
+import logging
+#from flask_ngrok import run_with_ngrok 
 from flask import Flask
 from app.routes import main as main_blueprint
 from config.config import LOGGING_LEVEL
-import logging
 
 def create_app():
     # Ensure Flask knows where to find templates
     app = Flask(__name__, template_folder="templates")
+    app.secret_key = os.urandom(24)  # or use a fixed string like 'your-secret-key'
+    #run_with_ngrok(app) 
     # Basic configuration
     app.config["LOGGING_LEVEL"] = LOGGING_LEVEL
     logging.basicConfig(level=LOGGING_LEVEL)
