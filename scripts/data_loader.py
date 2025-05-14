1a8cd37a0d7365ce6d6f1070c04f224233346ce4
umarhunter <markufaruque@gmail.com>
2025-05-14 13:14:12 -0400

    fix: remove comments

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -7,13 +7,11 @@ import pandas as pd
 
 def load_medicare_data():
     # Load the data from the csv files
-    # Get the project root directory (2 levels up from notebook)
     PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 
     # Construct the path to the data file
-    # data_path = os.path.join(PROJECT_ROOT, 'vdba', 'data', 'insurance', 'medicare', '2022', 'medicare.csv')
-    data_path = os.path.join(PROJECT_ROOT, 'vdba', 'data', 'processed', 'sample_ny_data.csv')
+    data_path = os.path.join(PROJECT_ROOT, 'vdba', 'data', 'insurance', 'medicare', '2022', 'medicare.csv')
+
     medicare_data = pd.read_csv(data_path, dtype='str')
-    # ny_data = medicare_data[medicare_data['Rndrng_Prvdr_State_Abrvtn'] == 'NY']
-    # ny_data_sample = ny_data.sample(n=50000, random_state=42)
+
     return medicare_data
